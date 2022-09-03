e1ff772121c63cacc3f47bce19ec7517cb6f6c38
ijaz sarwar <ijaz.sarwar@devorbis.com>
2022-09-03 15:35:38 +0500

    updatesss

--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -10,7 +10,7 @@ function Header() {
         <img src="logo.PNG" width={170} height={102} />
       </div>
       <div style={{ width: '88%' }}>
-        <h1 style={{fontWeight:'bold'}}>KPBrowser: A Dynamic Web Platform for Genome, Proteome and Metabolome Visualization and Analysis of Klebsiella pneumonia</h1>
+        <h1 style={{fontWeight:'bold'}}>KPBrowser: A Dynamic Web Platform for Genome, Proteome and Metabolome Visualization and Analysis of <i>Klebsiella pneumoniae</i></h1>
       </div>
     </div>
 
--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -4,20 +4,15 @@ function Home() {
   return (
     <div style={{ width: '70%', display: 'inline-block', margin: 40 }}>
       <img style={{ marginTop: 20, marginBottom: 20 }} src="home.jpg" alt="Smiley face" width="100%" height="auto" />
-
-      <p style={{ textAlign: 'start' }}>Metabolomics is the newly emerging field of “omics” research. Each organism has a unique and chemically distinct metabolome.
-        In field of metabolomics, there is need to create the metabolome database for gram positive bacterial species. Gram positive
-        bacteria have a thick layer of cell wall which contains several surface proteins and enzymes for the cell wall sorting and
-        targeting mechanism. Many essential details of cellular metabolism and numerous metabolic pathways of gram positive bacteria
-        have been worked out through careful biochemical studies and extensive metabolic manipulations. Gram Positive Bacterial
-        Metabolite database (GPBMDB) will be design along with their biological roles like disease associations, chemical reaction and
-        metabolic pathways. Initially, focus on three gram positive bacteria (Staphylococcus aureus, Streptococcus pneumonaie and
-        Bacillus subtilis) to create metabolite database. The pathways, chemical reactions, structure and physical properties of
-        metabolites will be collected from online databases like PubChem, KEGG, ChEBI, NCBI and from metabolite libraries.
-        An online user-friendly database will be constructed by using computer programs like MySQL and JAVA. The database will create
-        to use Ruby and Rails development tool for web applications in the database. This GPBMDB will provide a platform which is
-        expected to be a richly annotated, web-accessible and user friendly metabolite database. On the discovery of new metabolites
-        the existing information will be updated present in the database. </p>
+<h1 style={{ textAlign: 'start' }}>Why KPBrowser is Needed</h1>
+      <p style={{ textAlign: 'start' }}>Gene discovery, functional analysis, and hypothesis-driven research in the domains of model organisms<i> Bacillus subtilis, Escherichia coli</i> and<i>  Streptococcus pneumoniae </i>have been greatly accelerated by annotation databases like SubtiWiki, EcoCyc and JBrowse. Therefore, it was unexpected that such a resource was not yet available for <i>Klebsiella pneumoniae</i>, an important opportunistic human disease that causes more than a million fatalities yearly. We have now thoroughly annotated the <i>K. pneumoniae</i> genome and proteome at an unparalleled degree of detail by taking use of current scientific, technical advancements and bioinformatic annotation tools. On the other hand, a wealth of information is already available for well-studied bacteria <i>K. pneumoniae </i> in scattered forms. Therefore, a separate, manually generated platform is essential to maintain oversight of the current knowledge in the field. 
+</p>
+<h1 style={{ textAlign: 'start' }}>KPBrowser: A Single Platform for Analytical Studies of <i>K. pneumoniae</i>  Strain Kp52.145</h1>
+      <p style={{ textAlign: 'start' }}> <i>Klebsiella pneumoniae</i> Browser (KPBrowser) is an online user-friendly web platform that has been designed in a precise way for the functional elements of gene and protein sequences of <i>K. pneumoniae</i>  strain Kp52.145. It has successfully determined a new genome annotation from the whole genome, gene and protein sequences by using different types of automated, analysis, prediction annotation tools of bioinformatics followed by database curation reflecting the advance studies in the science and research field.</p>
+<h1 style={{ textAlign: 'start' }}>KPBrowser: A Database for <i>K. pneumoniae</i>  Strain Kp52.145</h1>
+      <p style={{ textAlign: 'start' }}>Initially, 100 gene and protein sequences were analyzed to create a genome database where users are allowed to browse encoded features along the promoter and primer sequences, operon prediction, gene structures, regulatory elements, repeat regions, protein characterization and their functional analysis, localization and family prediction, motif structures and other useful properties. Additionally, the browser provides direct linking to bioinformatics tools used for its analysis.</p>
+<h1 style={{ textAlign: 'start' }}>KPBrowser: An Up To Date Dynamic Web Platform</h1>
+      <p style={{ textAlign: 'start' }}>All the annotated information is accessible in this online resource providing one of the most well organized platforms for <i>K. pneumoniae</i> researchers. New features and elements may still be elucidated in the future and the existing information will be updated present in the database twice a year.</p>
 
     </div>
   )
